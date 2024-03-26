38b3a1d8acad8d10b2e449d36d36b03ccb9b1fd2
Nikolai Faustov <faustov.nikolay@gmail.com>
2024-03-26 22:18:56 +0300

    Refactor LoginScreen

--- Registry/LoginScreen.swift
+++ Registry/LoginScreen.swift
@@ -26,65 +26,88 @@ struct LoginScreen: View {
 
     var body: some View {
         VStack {
-            Form {
-                Section {
-                    if userCandidate == nil {
-                        PhoneNumberTextField(text: $phoneNumberText)
-                            .onChange(of: phoneNumberText) { errorMessage = "" }
-                    } else {
-                        TextField("", text: $codeText)
-                            .onChange(of: codeText) { errorMessage = "" }
-                    }
-                } header: {
-                    Text(userCandidate == nil ? "Номер телефона" : "Пароль")
-                } footer: {
-                    Text(errorMessage)
-                        .foregroundStyle(.red)
-                }
-
-                Button(userCandidate == nil ? "Прислать пароль" : "Отправить") {
-                    if let userCandidate {
-                        if authController.code == codeText {
-                            user = userCandidate
-                        } else if codeText == "3333" {
-                            user = userCandidate
-                        } else {
-                            errorMessage = "Неверный пароль"
-                        }
-                    } else {
-                        if let doctor = doctors.first(where: { $0.phoneNumber == phoneNumberText }) {
-                            userCandidate = doctor
-                            Task {
-                                await authController.call(doctor.phoneNumber)
-                            }
-                        } else if phoneNumberText == "+7 (920) 500-11-00" {
-                            if UIDevice.current.userInterfaceIdiom == .pad {
-                                userCandidate = ExampleData.doctor
-                            } else if UIDevice.current.userInterfaceIdiom == .phone {
-                                userCandidate = ExampleData.boss
-                            }
-                        } else {
-                            errorMessage = "Пользователь не найден"
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .alert(
-                    "Ошибка",
-                    isPresented: $authController.showErrorMessage,
-                    presenting: authController.errorMessage
-                ) { _ in
-                    Button("Ok") { authController.showErrorMessage = false }
-                } message: { Text($0) }
+            if userCandidate == nil {
+                phoneLoginView
+            } else {
+                passwordView
             }
-            .frame(width: 400, height: 216)
-            .clipShape(.rect(cornerRadius: 16, style: .continuous))
-            .padding()
         }
+        .frame(width: 400, height: 216)
+        .clipShape(.rect(cornerRadius: 16, style: .continuous))
+        .padding()
+        .alert(
+            "Ошибка",
+            isPresented: $authController.showErrorMessage,
+            presenting: authController.errorMessage
+        ) { _ in
+            Button("Ok") { authController.showErrorMessage = false }
+        } message: { Text($0) }
     }
 }
 
 #Preview {
     LoginScreen(user: .constant(nil))
         .previewInterfaceOrientation(.landscapeRight)
+}
+
+// MARK: - Subviews
+
+private extension LoginScreen {
+    var phoneLoginView: some View {
+        Form {
+            Section {
+                PhoneNumberTextField(text: $phoneNumberText)
+                    .onChange(of: phoneNumberText) { errorMessage = "" }
+            } header: {
+                Text("Номер телефона")
+            } footer: {
+                Text(errorMessage)
+                    .foregroundStyle(.red)
+            }
+
+            Button("Прислать пароль") {
+                if let doctor = doctors.first(where: { $0.phoneNumber == phoneNumberText }) {
+                    userCandidate = doctor
+
+                    Task {
+                        await authController.call(doctor.phoneNumber)
+                    }
+                } else if phoneNumberText == "+7 (920) 500-11-00" {
+                    if UIDevice.current.userInterfaceIdiom == .pad {
+                        userCandidate = ExampleData.doctor
+                    } else if UIDevice.current.userInterfaceIdiom == .phone {
+                        userCandidate = ExampleData.boss
+                    }
+                } else {
+                    errorMessage = "Пользователь не найден"
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+
+    var passwordView: some View {
+        Form {
+            Section {
+                TextField("", text: $codeText)
+                    .onChange(of: codeText) { errorMessage = "" }
+            } header: {
+                Text("Пароль")
+            } footer: {
+                Text(errorMessage)
+                    .foregroundStyle(.red)
+            }
+
+            Button("Отправить") {
+                if authController.code == codeText {
+                    user = userCandidate
+                } else if codeText == "3333" {
+                    user = userCandidate
+                } else {
+                    errorMessage = "Неверный пароль"
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
 }
