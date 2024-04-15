d90bc072a2af2d69595e646639347291225fc35e
Nikolai Faustov <faustov.nikolay@gmail.com>
2024-04-15 12:05:51 +0300

    Modified PatientsChart

--- Registry/Views/UIComponents/ReportingViews/PatientsChart.swift
+++ Registry/Views/UIComponents/ReportingViews/PatientsChart.swift
@@ -18,6 +18,8 @@ struct PatientsChart: View {
     @State private var selectedPeriod: StatisticsPeriod = .day
     @State private var patientsCount: Int = 0
     @State private var schedules: [DoctorSchedule] = []
+    @State private var isLoadingPatientsForPeriod: Bool = true
+    @State private var isLoadingPatientsCount: Bool = true
 
     // MARK: -
 
@@ -30,19 +32,46 @@ struct PatientsChart: View {
             }
             .pickerStyle(.segmented)
             .onChange(of: selectedPeriod) { _, newValue in
+                isLoadingPatientsForPeriod = true
                 Task {
                     fetchSchedules()
                 }
             }
 
-            LabeledContent("Пациентов за \(selectedPeriod.rawValue.lowercased())", value: "\(scheduledPatients.count)")
-            LabeledContent("Новые", value: "\(newPatients.count)")
-            Text("Всего в базе: \(patientsCount)")
+            LabeledContent("Пациентов за \(selectedPeriod.rawValue.lowercased())") {
+                if isLoadingPatientsForPeriod {
+                    CircularProgressView()
+                        .padding(.horizontal)
+                } else {
+                    Text("\(scheduledPatients.count)")
+                }
+            }
+            LabeledContent("Новые") {
+                if isLoadingPatientsForPeriod {
+                    CircularProgressView()
+                        .padding(.horizontal)
+                } else {
+                    Text("\(newPatients.count)")
+                }
+            }
+            LabeledContent("Всего в базе") {
+                if isLoadingPatientsCount {
+                    CircularProgressView()
+                        .padding(.horizontal)
+                } else {
+                    Text("\(patientsCount)")
+                }
+            }
         }
         .task {
-            let patientsDescriptor = FetchDescriptor<Patient>()
-            if let count = try? modelContext.fetchCount(patientsDescriptor) {
-                patientsCount = count
+            Task {
+                let patientsDescriptor = FetchDescriptor<Patient>()
+
+                if let count = try? modelContext.fetchCount(patientsDescriptor) {
+                    patientsCount = count
+                }
+
+                isLoadingPatientsCount = false
             }
 
             Task {
@@ -82,5 +111,7 @@ private extension PatientsChart {
         if let schedules = try? modelContext.fetch(descriptor) {
             self.schedules = schedules
         }
+
+        isLoadingPatientsForPeriod = false
     }
 }
