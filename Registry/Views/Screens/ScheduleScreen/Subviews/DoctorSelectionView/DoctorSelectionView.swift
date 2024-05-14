d6ec24dbba6b5f35f3b6ca33474db6096381711f
Nikolai Faustov <faustov.nikolay@gmail.com>
2024-05-14 10:39:53 +0300

    Disabled ability to add second doctor schedule for a day

--- Registry/Views/Screens/ScheduleScreen/Subviews/DoctorSelectionView/DoctorSelectionView.swift
+++ Registry/Views/Screens/ScheduleScreen/Subviews/DoctorSelectionView/DoctorSelectionView.swift
@@ -20,31 +20,46 @@ struct DoctorSelectionView: View {
 
     @State private var searchText: String = ""
     @State private var selectedDoctor: Doctor?
+    @State private var doctors: [Doctor] = []
 
     // MARK: -
 
     var body: some View {
         NavigationStack {
-            if let searchedDoctors = try? modelContext.fetch(searchDescriptor) {
-                List(searchedDoctors) { doctor in
-                    Button {
-                        selectedDoctor = doctor
-                    } label: {
-                        DoctorView(doctor: doctor, presentation: .listRow)
-                    }
+            List(doctors) { doctor in
+                Button {
+                    selectedDoctor = doctor
+                } label: {
+                    DoctorView(doctor: doctor, presentation: .listRow)
                 }
-                .listStyle(.inset)
-                .searchable(
-                    text: $searchText,
-                    placement: .navigationBarDrawer(displayMode: .always)
-                )
-                .sheet(item: $selectedDoctor) {
-                    CreateDoctorScheduleView(doctor: $0, date: date) {
-                        dismiss()
-                    }
-                }
-                .sheetToolbar("Выберите специалиста")
+                .disabled(alreadyHasTodaySchedule(doctor))
             }
+            .listStyle(.inset)
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always)
+            )
+            .sheet(item: $selectedDoctor) {
+                CreateDoctorScheduleView(doctor: $0, date: date) {
+                    dismiss()
+                }
+            }
+            .task {
+                let predicate = #Predicate<Doctor> { doctor in
+                    searchText.isEmpty ? true :
+                    doctor.secondName.localizedStandardContains(searchText) ||
+                    doctor.firstName.localizedStandardContains(searchText) ||
+                    doctor.patronymicName.localizedStandardContains(searchText)
+                }
+                let descriptor = FetchDescriptor<Doctor>(predicate: predicate)
+
+                if let searchedDoctors = try? modelContext.fetch(descriptor) {
+                    doctors = searchedDoctors
+                        .sorted(by: { $0.schedules?.count ?? 0 > $1.schedules?.count ?? 0 })
+                        .sorted(by: { !alreadyHasTodaySchedule($0) && alreadyHasTodaySchedule($1) })
+                }
+            }
+            .sheetToolbar("Выберите специалиста")
         }
     }
 }
@@ -56,15 +71,11 @@ struct DoctorSelectionView: View {
 // MARK: - Calculations
 
 private extension DoctorSelectionView {
-    var searchDescriptor: FetchDescriptor<Doctor> {
-        let predicate = #Predicate<Doctor> { doctor in
-            searchText.isEmpty ? true :
-            doctor.secondName.localizedStandardContains(searchText) ||
-            doctor.firstName.localizedStandardContains(searchText) ||
-            doctor.patronymicName.localizedStandardContains(searchText)
-        }
-        let descriptor = FetchDescriptor<Doctor>(predicate: predicate)
+    func alreadyHasTodaySchedule(_ doctor: Doctor) -> Bool {
+        guard let schedules = doctor.schedules else { return false }
 
-        return descriptor
+        return !schedules
+            .filter { Calendar.current.isDate(date, inSameDayAs: $0.starting) }
+            .isEmpty
     }
 }
