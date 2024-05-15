1311d44dfd1ea7688e41a72d812454cfe6adf736
Nikolai Faustov <faustov.nikolay@gmail.com>
2024-05-15 09:51:35 +0300

    Fixed day schedules sorting

--- Registry/Views/Screens/ScheduleScreen/ScheduleScreen.swift
+++ Registry/Views/Screens/ScheduleScreen/ScheduleScreen.swift
@@ -107,7 +107,7 @@ private extension ScheduleScreen {
         let startOfDay = Calendar.current.startOfDay(for: scheduleController.date)
         let endOfDay = Calendar.current.startOfDay(for: scheduleController.date.addingTimeInterval(86_400))
         let schedulesPredicate = #Predicate<DoctorSchedule> { $0.starting > startOfDay && $0.ending < endOfDay }
-        let descriptor = FetchDescriptor(predicate: schedulesPredicate, sortBy: [SortDescriptor(\.starting, order: .forward)])
+        let descriptor = FetchDescriptor(predicate: schedulesPredicate, sortBy: [SortDescriptor(\.starting, order: .forward), SortDescriptor(\.ending, order: .forward)])
 
         guard let schedules = try? modelContext.fetch(descriptor) else { return [] }
 
